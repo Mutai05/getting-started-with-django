55a853ae16780b27adb8d5524df0fa5c5aafcda6
Mutai05 <kelvok2014@gmail.com>
2024-01-02 00:23:54 +0300

    Updated the views to fetch data from the database using models and render templates.

--- my_project/poll_project/polls/views.py
+++ my_project/poll_project/polls/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, this is the polls index.")
+    latest_polls = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_polls': latest_polls}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    return HttpResponse("You're voting on question %s." % question_id)
